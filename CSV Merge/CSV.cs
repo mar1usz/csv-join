c80209c9b38ca3db426e27029126e4b091e8155c
Mariusz <mariopl@poczta.onet.eu>
2021-01-30 15:05:10 +0100

    add comments, refactor

--- CSV Merge/CSV.cs
+++ CSV Merge/CSV.cs
@@ -31,12 +31,15 @@ namespace CSV_Merge
             else
             {
                 LinkedList<string> tempFilepaths = new LinkedList<string>();
+
+                // Merge first two
                 using (FileStream tempStream = File.OpenWrite(Path.GetTempFileName()))
                 {
                     Merge(filepaths[0], filepaths[1], tempStream, culture);
                     tempFilepaths.AddLast(tempStream.Name);
                 }
 
+                // Merge in-between if any
                 foreach (string filepath in filepaths.Skip(2).SkipLast(1))
                 {
                     using (FileStream tempStream = File.OpenWrite(Path.GetTempFileName()))
@@ -48,6 +51,7 @@ namespace CSV_Merge
                     }
                 }
 
+                // Merge last one to output
                 Merge(tempFilepaths.Last.Value, filepaths.Last(), output, culture);
                 if (File.Exists(tempFilepaths.Last.Value))
                     File.Delete(tempFilepaths.Last.Value);
@@ -56,50 +60,56 @@ namespace CSV_Merge
 
         public static void Merge(string filepath1, string filepath2, Stream output, CultureInfo culture)
         {
+            using (var reader1 = new StreamReader(filepath1))
+            using (var csv1 = new CsvReader(reader1, culture))
+            using (var reader2 = new StreamReader(filepath2))
+            using (var csv2 = new CsvReader(reader2, culture))
+            {
+                csv1.Configuration.TrimOptions = TrimOptions.Trim;
+                csv2.Configuration.TrimOptions = TrimOptions.Trim;
+
+                using (var dr1 = new CsvDataReader(csv1))
+                using (var dr2 = new CsvDataReader(csv2))
+                {
+                    DataTable dt1 = new DataTable();
+                    DataTable dt2 = new DataTable();
+                    dt1.Load(dr1);
+                    dt2.Load(dr2);
+
+                    DataColumn[] dt1PrimaryKey = dt1.Columns.Cast<DataColumn>().Select(dt1col => dt1col).Where(dt1col => dt2.Columns.Contains(dt1col.ColumnName)).ToArray();
+                    DataColumn[] dt2PrimaryKey = dt2.Columns.Cast<DataColumn>().Select(dt2col => dt2col).Where(dt2col => dt1.Columns.Contains(dt2col.ColumnName)).ToArray();
+                    dt1.PrimaryKey = dt1PrimaryKey;
+                    dt2.PrimaryKey = dt2PrimaryKey;
+                    dt1.Merge(dt2);
+
+                    WriteCsv(dt1, output, culture);
+                }
+            }
+        }
+
+        private static void WriteCsv(DataTable dt, Stream output, CultureInfo culture)
+        {
             using (var writer = new StreamWriter(output))
             using (var csvWriter = new CsvWriter(writer, culture))
             {
                 csvWriter.Configuration.TrimOptions = TrimOptions.Trim;
 
-                using (var reader1 = new StreamReader(filepath1))
-                using (var csv1 = new CsvReader(reader1, culture))
-                using (var reader2 = new StreamReader(filepath2))
-                using (var csv2 = new CsvReader(reader2, culture))
+                int rows = dt.Rows.Count;
+                int cols = dt.Columns.Count;
+
+                for (int i = 0; i < cols; i++)
                 {
-                    csv1.Configuration.TrimOptions = TrimOptions.Trim;
-                    csv2.Configuration.TrimOptions = TrimOptions.Trim;
+                    csvWriter.WriteField(dt.Columns[i]);
+                }
+                csvWriter.NextRecord();
 
-                    using (var dr1 = new CsvDataReader(csv1))
-                    using (var dr2 = new CsvDataReader(csv2))
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
                     {
-                        DataTable dt1 = new DataTable();
-                        DataTable dt2 = new DataTable();
-                        dt1.Load(dr1);
-                        dt2.Load(dr2);
-
-                        DataColumn[] dt1PrimaryKey = dt1.Columns.Cast<DataColumn>().Select(dt1col => dt1col).Where(dt1col => dt2.Columns.Contains(dt1col.ColumnName)).ToArray();
-                        DataColumn[] dt2PrimaryKey = dt2.Columns.Cast<DataColumn>().Select(dt2col => dt2col).Where(dt2col => dt1.Columns.Contains(dt2col.ColumnName)).ToArray();
-                        dt1.PrimaryKey = dt1PrimaryKey;
-                        dt2.PrimaryKey = dt2PrimaryKey;
-                        dt1.Merge(dt2);
-
-                        int rows = dt1.Rows.Count;
-                        int cols = dt1.Columns.Count;
-                        for (int i = 0; i < cols; i++)
-                        {
-                            csvWriter.WriteField(dt1.Columns[i]);
-                        }
-                        csvWriter.NextRecord();
-
-                        for (int i = 0; i < rows; i++)
-                        {
-                            for (int j = 0; j < cols; j++)
-                            {
-                                csvWriter.WriteField(dt1.Rows[i][dt1.Columns[j]]);
-                            }
-                            csvWriter.NextRecord();
-                        }
+                        csvWriter.WriteField(dt.Rows[i][dt.Columns[j]]);
                     }
+                    csvWriter.NextRecord();
                 }
             }
         }
