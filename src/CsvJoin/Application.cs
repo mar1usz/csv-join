ec5a1bb643d9f781e176f2890a436b5e9db2c4c5
Mariusz <mariopl@poczta.onet.eu>
2021-04-17 16:00:45 +0200

    fix formatting in Application.cs

--- src/CsvJoin/Application.cs
+++ src/CsvJoin/Application.cs
@@ -42,6 +42,7 @@ namespace CsvJoin
                 Data Source={directory};
                 OLE DB Services=-1;
                 Extended Properties=""text;Excel 16.0;HDR=YES;IMEX=1""";
+
             var output = Console.OpenStandardOutput();
 
             await _executor.ExecuteSqlAsync(
