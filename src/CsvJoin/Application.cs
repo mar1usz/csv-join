261a0c25b2718a3d98d579627dbf51dca5a5b012
Mariusz Stępień <mariusz.stepien@me.pl>
2021-11-04 19:56:06 +0100

    escape connectionString using \

--- src/CsvJoin/Application.cs
+++ src/CsvJoin/Application.cs
@@ -57,7 +57,7 @@ namespace CsvJoin
             return "Provider=Microsoft.ACE.OLEDB.16.0;" +
                 $"Data Source={directory};" +
                 "OLE DB Services=-1;" +
-                @"Extended Properties=""text;Excel 16.0;HDR=YES;IMEX=1""";
+                "Extended Properties=\"text;Excel 16.0;HDR=YES;IMEX=1\"";
         }
     }
 }
