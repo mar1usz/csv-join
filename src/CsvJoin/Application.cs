8100143ac9a2991b289d691510bea2006855f859
Mariusz Stępień <mariusz.stepien@me.pl>
2021-11-04 11:05:58 +0100

    Revert "remove named argument"
    This reverts commit 544d10b042d98732660de8c38e3988f497321c4e.

--- src/CsvJoin/Application.cs
+++ src/CsvJoin/Application.cs
@@ -51,7 +51,7 @@ namespace CsvJoin
                 connectionString,
                 output);
 
-            await _saver.SaveSqlAsync(sql, SqlSaverFilePath);
+            await _saver.SaveSqlAsync(sql, filePath: SqlSaverFilePath);
         }
 
         private static string GetConnectionString(string directory)
