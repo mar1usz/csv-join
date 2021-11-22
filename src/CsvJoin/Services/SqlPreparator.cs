95f9b9f109f50a68519d6a5d622a09daef040f14
Mariusz Stępień <mariopl@poczta.onet.eu>
2021-11-22 11:10:38 +0100

    rename methods

--- src/CsvJoin/Services/SqlPreparator.cs
+++ src/CsvJoin/Services/SqlPreparator.cs
@@ -29,8 +29,8 @@ namespace CsvJoin.Services
             string[] tables = GetTableNames(fileNames);
             string[][] columns = GetColumnNames(directory, fileNames);
 
-            string[] allColumns = GetAllColumns(columns);
-            string[] joinColumns = GetJoinColumns(columns);
+            string[] allColumns = GetAllColumnNames(columns);
+            string[] joinColumns = GetJoinColumnNames(columns);
 
             var sql = new StringBuilder();
 
@@ -82,8 +82,8 @@ namespace CsvJoin.Services
             string[] tables = GetTableNames(fileNames);
             string[][] columns = GetColumnNames(directory, fileNames);
 
-            string[] allColumns = GetAllColumns(columns);
-            string[] joinColumns = GetJoinColumns(columns);
+            string[] allColumns = GetAllColumnNames(columns);
+            string[] joinColumns = GetJoinColumnNames(columns);
 
             var sql = new StringBuilder();
 
@@ -156,10 +156,10 @@ namespace CsvJoin.Services
                 .ToArray();
         }
 
-        private string[] GetAllColumns(string[][] columns) =>
-           columns[0].Union(columns[1]).ToArray();
+        private string[] GetAllColumnNames(string[][] columnNames) =>
+           columnNames[0].Union(columnNames[1]).ToArray();
 
-        private string[] GetJoinColumns(string[][] columns) =>
-            columns[0].Intersect(columns[1]).ToArray();
+        private string[] GetJoinColumnNames(string[][] columnNames) =>
+            columnNames[0].Intersect(columnNames[1]).ToArray();
     }
 }
