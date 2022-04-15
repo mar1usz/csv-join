c54baebf058028eccb1c1d42ae5d9ceda95cc306
mariusz96 <62397363+mariusz96@users.noreply.github.com>
2022-04-15 09:35:56 +0200

    fix formatting

--- src/CsvJoin/Services/SqlPreparator.cs
+++ src/CsvJoin/Services/SqlPreparator.cs
@@ -29,6 +29,7 @@ namespace CsvJoin.Services
             var sql = new StringBuilder();
 
             var command = GetCommand(directory, fileNames);
+
             string allColumnsFirst = command.AllColumnNames.First();
             sql.AppendFormat("SELECT [{0}].[{1}]",
                 command.ColumnNames[0].Contains(allColumnsFirst)
@@ -81,6 +82,7 @@ namespace CsvJoin.Services
             var sql = new StringBuilder();
 
             var command = GetCommand(directory, fileNames);
+
             string allColumnsFirst = command.AllColumnNames.First();
             sql.AppendFormat("SELECT [{0}].[{1}]",
                 command.ColumnNames[1].Contains(allColumnsFirst)
